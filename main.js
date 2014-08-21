f737cf70efa907262c2cb918ce94b8ff8fff34a6
lc-luis <luisfernandezmolina@iMac-de-Luis.local>
2014-08-21 23:11:30 +0200

    Update

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-var dibujo, ctx, fondo;
 function inicio()
 {
 	dibujo = document.getElementById("miCanvas");
 	ctx = dibujo.getContext("2d");
 	//var interval = window.setInterval(cargarMedia,1000/55);
+	//Ejecutar Funciones
+	cargarImagenes();
+	dibujarFondo();
+	dibujarFondoTrasparente();
+	dibujarTexto();
 }
+
+//Variables
+var dibujo, ctx, fondo;
+
+//Fin Variables
 
 function cargarImagenes()
 {
@@ -17,11 +26,20 @@ function dibujarFondo()
 	ctx.drawImage(fondo,0,0,400,200);
 }
 
-/*function cargarMedia()
+/*function dibujarTexto()
 {
-	cargarImagenes();
+	
 }*/
 
-//Ejecutar Funciones
-cargarImagenes();
-dibujarFondo();
+function dibujarFondoTrasparente()
+{
+	ctx.save();
+	ctx.globalAlpha = 0.7;
+	ctx.beginPath();
+	ctx.rect(0, 0, ctx.width, ctx.height);
+	ctx.fillStyle = "grey";
+	ctx.fill();
+	ctx.restore();
+	//Borrar el rectangulo
+	//context.clearRect(0, 0, ctx.width, ctx.height);
+}
